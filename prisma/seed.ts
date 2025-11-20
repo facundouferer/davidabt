fc9ed3ec0e877532d1f03bf778495abacce0c7b9
Facundo Uferer <juanfacundouf@gmail.com>
2025-11-20 00:08:35 -0300

    feat: Add Prisma ORM with User model, initial seeding, and related dependencies for database management.

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = await bcrypt.hash("admin", 10);
+  const user = await prisma.user.upsert({
+    where: { username: "admin" },
+    update: {},
+    create: {
+      username: "admin",
+      email: "admin@davidabt.com",
+      password,
+      role: "admin",
+    },
+  });
+  console.log({ user });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
